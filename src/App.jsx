07f385560674ddebdb481a076be9e4afea53fe89
AebiM <162427495+AebiM@users.noreply.github.com>
2024-11-20 12:55:46 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Typography from "@mui/material/Typography";
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h2">Hello React & MUI</Typography>
+    </div>
+  );
+}
+
+export default App;
