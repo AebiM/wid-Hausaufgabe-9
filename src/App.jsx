659ed8a22d0b406ecc29ebb9bd4b86dcec239dfb
AebiM <manuel.aebi@students.fhnw.ch>
2024-11-20 13:19:31 +0100

    Farm life=good life

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Typography from "@mui/material/Typography";
+import { VegaLite } from "react-vega";
+import file from "./data/file.json";
 
 function App() {
   return (
     <div>
-      <Typography variant="h2">Hello React & MUI</Typography>
+      <Typography variant="h2">Vite + React</Typography>
+      <Typography variant="h6">
+        Durchschnittliche Anzahl Gegessener Tiere im Jahr
+      </Typography>
+
+      <VegaLite spec={file} />
     </div>
   );
 }
